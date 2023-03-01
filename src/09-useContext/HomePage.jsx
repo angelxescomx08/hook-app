c699e0ca5382070b270e2df4d50947cbaf812684
angelxescomx08 <jhernandezr1313@alumno.ipn.mx>
2023-03-01 16:12:54 -0600

    use context

--- src/09-useContext/HomePage.jsx
+++ src/09-useContext/HomePage.jsx
@@ -1,8 +1,29 @@
+import { useContext } from "react"
+import { UserContext } from "./context/UserContext"
+
 export const HomePage = () => {
+
+    const { user, setUser } = useContext(UserContext)
+
     return (
         <>
-            <h1>HomePage</h1>
+            <h1>HomePage <small>{user?.name}</small></h1>
             <hr />
+            <pre>
+                {
+                    JSON.stringify(user, null, 3)
+                }
+            </pre>
+            <button
+                className="btn btn-primary"
+                onClick={() => setUser({
+                    id: 123,
+                    name: 'José Ángel',
+                    email: 'email@gmail.com'
+                })}
+            >
+                Establecer usuario
+            </button>
         </>
     )
 }
